a4a0a8e88c2d65405f81290a5251bc266d866ba1
Valeriy Kovshov <34447913+prohladenn@users.noreply.github.com>
2025-05-22 22:11:46 +0000

    Добавить зависимости для Google Services и Firebase Authentication в build.gradle.kts; изменить права доступа для gradlew

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,6 +47,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
